25e2ecc3a3a6a8acc26974b8b0320cb101686cee
IcedZ <gatorchris@gmail.com>
2020-12-23 15:54:08 -0500

    Improved controls

--- Telescope.ino
+++ Telescope.ino
@@ -14,15 +14,13 @@
 // the other on motor pins 2 & 4. The step angle is 5.625/64 and the
 // operating Frequency is 100pps. Current draw is 92mA.
 
-Using a DPDT (on-off-on) switch to determine motor direction.  
-"On" poles connected to pins 2, 3; switch set to "INPUT_PULLUP"
-"Off" pole connected to ground
+Two momentary push buttons; one for CCW, one for CW rotation.
+Potentiometer for setting speed.
 
-Using momentary push button for on/off operation of the motor.
-Connected to pin 4 and ground. Pin 4 set to "INPUT_PULLUP"
+Using momentary push buttons for on/off operation of the motor.
+Set pushbutton pins to "INPUT_PULLUP"
 
  ******************************************/
-//#include <Stepper.h>
 #include <AccelStepper.h>
 
 
@@ -35,10 +33,12 @@ int motorPin3 = 10;    // Yellow - 28BYJ48 pin 3
 int motorPin4 = 11;    // Orange - 28BYJ48 pin 4
                         // Red    - 28BYJ48 pin 5 (VCC)
 #define MotorInterfaceType 8
+//#define maxSpeed 1200     // This will be maxspeed once I implement potentiometer
+#define maxSpeed 600
 
 // initialize the stepper library on pins 8 through 11:
-AccelStepper forwardStepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin2, motorPin3, motorPin4);
-AccelStepper reverseStepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3, motorPin2, motorPin4);
+// AccelStepper forwardStepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin2, motorPin3, motorPin4); // This one won't go in reverse with negative speeds.  Doesn't work.
+AccelStepper myStepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3, motorPin2, motorPin4);
 int stepCount = 0;  // number of steps the motor has taken
 
 // Declare variables for the switch pins.
@@ -46,48 +46,38 @@ int stepCount = 0;  // number of steps the motor has taken
 int cwPin = 2;
 // Anti-Clockwise Pin
 int ccwPin = 3;
-// On Button
-int powerPin = 4;
-
-//int motorSpeed = 1200;  //variable to set stepper speed
-//int count = 0;          // count of stepsmade
-//int countsperrev = 512; // number of steps per full revolution
-//int lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001};
+// Speed Control Pin (potentiometer) - needs to be analog pin.
+int speedPin = 4;
 
 void setup() {
   //declare the motor pins as outputs
-//  pinMode(motorPin1, OUTPUT);
-//  pinMode(motorPin2, OUTPUT);
-//  pinMode(motorPin3, OUTPUT);
-//  pinMode(motorPin4, OUTPUT);
+    // I don't think this is needed; pretty sure AccelStepper does this? Need to verify
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
   Serial.begin(9600);
 
   // Set switch pins as inputs and pull-up
   pinMode(cwPin, INPUT_PULLUP);
   pinMode(ccwPin, INPUT_PULLUP);
-  pinMode(powerPin, INPUT_PULLUP);
+  //pinMode(speedPin, INPUT_PULLUP);
 
-  //int motorSpeed = 500;
-  //myStepper.setSpeed(motorSpeed);
-  forwardStepper.setMaxSpeed(1000);
-  reverseStepper.setMaxSpeed(1000);
+  myStepper.setMaxSpeed(maxSpeed);
 }
 
 //////////////////////////////////////////////////////////////////////////////
 void loop(){
 
-  while (!digitalRead(powerPin)) {
-    if (!digitalRead(cwPin)) {
-      forwardStepper.setSpeed(700);
-      forwardStepper.runSpeed();
-    }
-    else if (!digitalRead(ccwPin)) {
-    reverseStepper.setSpeed(700);
-    reverseStepper.runSpeed();
+  if (!digitalRead(cwPin)) {
+    myStepper.setSpeed(maxSpeed);
+    myStepper.runSpeed();
   }
-    else {
-      delay(10);
-    }
-
-}
+  else if (!digitalRead(ccwPin)) {
+    myStepper.setSpeed(maxSpeed * -1);
+    myStepper.runSpeed();
+  }
+  else {
+    delay(10);
+  }
 }
